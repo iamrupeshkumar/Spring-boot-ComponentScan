144f2d8d5634bdefa80c0187a00ee418864351dc
Rupesh Kumar <rupeshkumar.sde@gmail.com>
2026-04-19 01:56:23 +0530

    using @Bean

--- src/main/java/com/example/demo/SpringBootComponentScanApplication.java
+++ src/main/java/com/example/demo/SpringBootComponentScanApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.example.demo.security.AppSecurity;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.example.demo","com.wallmart"})
@@ -10,6 +13,13 @@ public class SpringBootComponentScanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootComponentScanApplication.class, args);
+	}
+	
+	@Bean
+	public AppSecurity createInstance() {
+		AppSecurity as = new AppSecurity();
+		// custom logic to secure our application
+		return as;
 	}
 
 }
